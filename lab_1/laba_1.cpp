f3f98a1025be6f2a9be87c98545aa1b704575762
margulanos <margulan.nursagatov@gmail.com>
2025-02-27 11:29:12 +0300

    commit4

--- lab_1/laba_1.cpp
+++ lab_1/laba_1.cpp
@@ -37,10 +37,10 @@ void worst_B();
 void worst_C();
 
 int main() {
-    worst_linear();
+    //worst_linear();
     //worst_binary();
     //average_linear();
-    //average_binary();
+    average_binary();
     //worst_vtupuyu();
     //worst_poumnomu();
     //worst_A();
@@ -53,11 +53,22 @@ return 0 ;
 
 //часть 1
 
-unsigned random_key(int N)
+
+unsigned random_key_index(int N)
 {
-    std::default_random_engine rng(time(NULL));
+    unsigned seed = 1001;
+    std::default_random_engine rng(seed);
     std::uniform_int_distribution <unsigned> dstr(0, N-1);
     return dstr(rng);
+}
+
+unsigned random_key(int N)
+{
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    //std::default_random_engine generator (time(NULL));
+    std::uniform_int_distribution <unsigned> dstr(0, N-1);
+    return dstr(generator);
 }
 
 void linear(int *arr, int N, int key)
@@ -73,7 +84,7 @@ void binary(int *arr, int N, int key)
     int r = N-1;
     while (l <= r)
     {
-        int m = (l + r) / 2;
+        int m = l + (r - l) / 2;
         if (key == arr[m]) return;
         else if (key > arr[m]) l = m + 1;
         else r = m - 1;
@@ -84,7 +95,7 @@ void binary(int *arr, int N, int key)
 unsigned time_worst_linear(int *arr, int N, int key)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0 ; --cnt)
+    for (unsigned cnt = 10000; cnt != 0 ; --cnt)
         linear(arr, N, key);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -106,7 +117,7 @@ unsigned time_worst_binary(int *arr, int N, int key)
 unsigned time_average_linear(int *arr, int N, int *key_arr)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 0; cnt < 100000 ; ++cnt)
+    for (unsigned cnt = 0; cnt < 10000 ; ++cnt)
         linear(arr, N, key_arr[cnt]);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -127,9 +138,8 @@ unsigned time_average_binary(int *arr, int N, int *key_arr)
 
 void worst_linear()
 {
-    std::ofstream outFile("worst_linear.txt");
-    std::ofstream outFileN("N_linear.txt");
-        
+    std::ofstream outFile("lab_1/worst_linear.txt");
+    std::ofstream outFileN("lab_1/N_linear.txt");
     for(int N = 100; N <= 1000000; N +=  50000)
     {
         int arr[N];
@@ -138,14 +148,14 @@ void worst_linear()
         outFileN << N << std::endl;
         outFile << time_worst_linear(arr, N, N) << std::endl;
     }
-    outFile.close();
     outFileN.close();
+    outFile.close();
 }
 
 void worst_binary()
 {
-    std::ofstream outFile("worst_binary.txt");
-    std::ofstream outFileN("N_binary.txt");
+    std::ofstream outFile("lab_1/worst_binary.txt");
+    std::ofstream outFileN("lab_1/N_binary.txt");
     for(int N = 100; N <= 1000000; N *= 1.6237)
     {
         int arr[N];
@@ -154,14 +164,14 @@ void worst_binary()
         outFileN << N << std::endl;
         outFile << time_worst_binary(arr, N, N) << std::endl;
     }
-    outFile.close();
     outFileN.close();
+    outFile.close();
 }
 
 void average_linear()
 {
     
-    std::ofstream outFile("average_linear.txt");
+    std::ofstream outFile("lab_1/average_linear.txt");
     for(int N = 100; N <= 1000000; N += 50000)
     {
         int arr[N];
@@ -178,8 +188,8 @@ void average_linear()
 
 void average_binary()
 {
-    std::ofstream outFile("average_binary.txt");
-    for(int N = 100; N <= 1000000; N *= 1.6237)
+    std::ofstream outFile("lab_1/average_binary.txt");
+    for(int N = 1000; N <= 1000000; N *= 1.6237)
     {
         int arr[N];
         for (int i = 0; i < N; i++)
@@ -237,7 +247,7 @@ unsigned time_worst_vtupuyu(int *arr, int N, int key)
 unsigned time_worst_poumnomu(int *arr, int N, int key)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0 ; --cnt)
+    for (unsigned cnt = 10000; cnt != 0 ; --cnt)
         poumnomu(arr, N, key);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -246,26 +256,30 @@ unsigned time_worst_poumnomu(int *arr, int N, int key)
 }
 void worst_vtupuyu()
 {
-    std::ofstream outFile("worst_vtupuyu.txt");
-    for(int N = 100; N <= 100000; N += 50000)
+    std::ofstream outFile("lab_1/worst_vtupuyu.txt");
+    std::ofstream outFileN("lab_1/N_vtupuyu.txt");
+    for(int N = 100; N <= 10000; N += 500)
     {
         int arr[N];
         for (int i = 0; i < N; i++)
             arr[i] = random_key(N);
+        
+        outFileN << N << std::endl;
         outFile << time_worst_vtupuyu(arr, N, N) << std::endl;
     }
+    outFileN.close();
     outFile.close();
 }
 
 void worst_poumnomu()
 {
-    std::ofstream outFile("worst_poumnomu.txt");
+    std::ofstream outFile("lab_1/worst_poumnomu.txt");
     for(int N = 100; N <= 1000000; N += 50000)
     {
         int arr[N];
         for (int i = 0; i < N; i++)
             arr[i] = i;
-        outFile << time_worst_poumnomu(arr, N, N) << std::endl;
+        outFile << time_worst_poumnomu(arr, N, 5*N) << std::endl;
     }
     outFile.close();
 }
@@ -332,7 +346,7 @@ int str_C(int *arr, int N, int key, int *arr0)
 unsigned time_A(int *arr, int N, int key)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0 ; --cnt)
+    for (unsigned cnt = 1000; cnt != 0 ; --cnt)
         str_A(arr, N, key);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -342,7 +356,7 @@ unsigned time_A(int *arr, int N, int key)
 unsigned time_B(int *arr, int N, int key)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0 ; --cnt)
+    for (unsigned cnt = 1000; cnt != 0 ; --cnt)
         str_B(arr, N, key);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -352,7 +366,7 @@ unsigned time_B(int *arr, int N, int key)
 unsigned time_C(int *arr, int N, int key, int *arr_0)
 {
     auto begin = std::chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0 ; --cnt)
+    for (unsigned cnt = 1000; cnt != 0 ; --cnt)
         str_C(arr, N, key, arr_0);
     auto end = std::chrono::steady_clock::now();
     auto time_span =
@@ -362,41 +376,41 @@ unsigned time_C(int *arr, int N, int key, int *arr_0)
 
 void worst_A()
 {
-    std::ofstream outFile("A_ner.txt");
+    std::ofstream outFile("lab_1/A.txt");
     for(int N = 100; N <= 1000000; N += 50000)
     {
         int arr[N];
         for (int i = 0; i < N; i++)
-            arr[i] = random_key(N);
+            arr[i] = random_key_index(N);
         int key = random_key(N);
-        if (key > N*0.3 && key < N*0.7) key = N/2;
+        //if (key > N*0.3 && key < N*0.7) key = N/2;
         outFile << time_A(arr, N, key) << std::endl;
     }
     outFile.close();
 }
 void worst_B()
 {
-    std::ofstream outFile("B_ner.txt");
+    std::ofstream outFile("lab_1/B.txt");
     for(int N = 100; N <= 1000000; N += 50000)
     {
         int arr[N];
         for (int i = 0; i < N; i++)
-            arr[i] = random_key(N);
+            arr[i] = random_key_index(N);
         int key = random_key(N);
-        if (key > N*0.3 && key < N*0.7) key = N/2;
+        //if (key > N*0.3 && key < N*0.7) key = N/2;
         outFile << time_B(arr, N, key) << std::endl;
     }
     outFile.close();
 }
 void worst_C()
 {
-    std::ofstream outFile("C_ner.txt");
-    for(int N = 100; N <= 100000; N += 50000)
+    std::ofstream outFile("lab_1/C_ner.txt");
+    for(int N = 100; N <= 1000000; N += 50000)
     {
         int arr[N];
         int arr_0[N] = {0};
         for (int i = 0; i < N; i++)
-            arr[i] = random_key(N);
+            arr[i] = random_key_index(N);
         int key = random_key(N);
         if (key > N*0.3 && key < N*0.7) key = N/2;
         outFile << time_C(arr, N, key, arr_0) << std::endl;
--- lab_1/laba_1.py
+++ lab_1/laba_1.py
@@ -9,7 +9,6 @@ def gr(path_N, path_time, nazvanie):
     N = []
     for i in data_N:
         N.append(int(i))
-    N = N[:28]
     N = np.array(N)
     with open(path_time, 'r') as file:
         data_time = file.read()
@@ -28,4 +27,15 @@ def gr(path_N, path_time, nazvanie):
     
     plt.savefig(nazvanie)
     plt.show()
-gr('C:/Users/HP/Desktop/proga/laby/N.txt', 'C:/Users/HP/Desktop/proga/laby/C_ner.txt','C_ner')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/worst_linear.txt', 'worst_linear')
+gr('C:/Users/HP/Desktop/proga/laby/N_binary.txt', 'C:/Users/HP/Desktop/proga/laby/worst_binary.txt', 'worst_binary')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/average_linear.txt', 'average_linear')
+gr('C:/Users/HP/Desktop/proga/laby/N_binary.txt', 'C:/Users/HP/Desktop/proga/laby/average_binary.txt', 'average_binary')
+gr('C:/Users/HP/Desktop/proga/laby/N_vtupuyu.txt', 'C:/Users/HP/Desktop/proga/laby/worst_vtupuyu.txt', 'worst_vtupuyu')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/worst_poumnomu.txt', 'worst_poumnomu')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/A.txt', 'A')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/B.txt', 'B')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/C.txt', 'C')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/A_ner.txt', 'A_ner')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/B_ner.txt', 'B_ner')
+gr('C:/Users/HP/Desktop/proga/laby/N_linear.txt', 'C:/Users/HP/Desktop/proga/laby/C_ner.txt', 'C_ner')
